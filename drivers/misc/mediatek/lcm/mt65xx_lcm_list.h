ed58b775d8adf27346b741cf79314c3f79e23769
melekpro <melekpro6@gmail.com>
2024-02-22 23:55:21 +0100

    Update mt65xx_lcm_list.h

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -3,6 +3,7 @@
 
 #include <lcm_drv.h>
 extern LCM_DRIVER st7701_fwgaplus_dsi_vdo_helitai_vp531_lcm_drv;
+extern LCM_DRIVER gc9503p_fwp_dsi_vdo_jt_ivo_ba2_lcm_drv;
 #ifdef BUILD_LK
 extern void mdelay(unsigned long msec);
 #endif
