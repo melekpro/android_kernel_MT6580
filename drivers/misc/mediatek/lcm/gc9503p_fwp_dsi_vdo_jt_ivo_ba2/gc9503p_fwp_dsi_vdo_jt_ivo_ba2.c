5b6430d67c77f97bd07ae42bd4c7a81ff511b8b7
melekpro <melekpro6@gmail.com>
2024-02-23 11:54:02 +0100

    Update gc9503p_fwp_dsi_vdo_jt_ivo_ba2.c

--- drivers/misc/mediatek/lcm/gc9503p_fwp_dsi_vdo_jt_ivo_ba2/gc9503p_fwp_dsi_vdo_jt_ivo_ba2.c
+++ drivers/misc/mediatek/lcm/gc9503p_fwp_dsi_vdo_jt_ivo_ba2/gc9503p_fwp_dsi_vdo_jt_ivo_ba2.c
@@ -190,61 +190,80 @@ static void lcm_set_util_funcs(const LCM_UTIL_FUNCS *util)
 
 static void lcm_get_params(LCM_PARAMS *params)
 {
-	memset(params, 0, sizeof(LCM_PARAMS));
+    int boot_mode = 0;
+    memset(params, 0, sizeof(LCM_PARAMS));
 
-	params->type = LCM_TYPE_DSI;
-	params->width = FRAME_WIDTH;
-	params->height = FRAME_HEIGHT;
+    params->type = LCM_TYPE_DSI;
 
-#ifndef BUILD_LK
-	params->physical_width = 62;		 // LCM_PHYSICAL_WIDTH/1000;
-	params->physical_height = 124;		 // LCM_PHYSICAL_HEIGHT/1000;
-	params->physical_width_um = 61877;	 // LCM_PHYSICAL_WIDTH; = sqrt((size*25.4)^2/(18^2+9^2))*9*1000
-	params->physical_height_um = 123754; // LCM_PHYSICAL_HEIGHT; = sqrt((size*25.4)^2/(18^2+9^2))*18*1000
-#endif
+    params->width = FRAME_WIDTH;
+    params->height = FRAME_HEIGHT;
+    params->physical_width = LCM_PHYSICAL_WIDTH/1000;
+    params->physical_height = LCM_PHYSICAL_HEIGHT/1000;
+    params->physical_width_um = LCM_PHYSICAL_WIDTH;
+    params->physical_height_um = LCM_PHYSICAL_HEIGHT;
+//    params->density = LCM_DENSITY;
 
-	// enable tearing-free
-	params->dbi.te_mode = LCM_DBI_TE_MODE_DISABLED;
-	params->dsi.mode = BURST_VDO_MODE; // SYNC_PULSE_VDO_MODE;//BURST_VDO_MODE;
+    params->dsi.mode = SYNC_PULSE_VDO_MODE;
+    params->dsi.switch_mode_enable = 0;
 
-	// DSI
-	/* Command mode setting */
-	params->dsi.LANE_NUM = LCM_TWO_LANE;
-	// The following defined the fomat for data coming from LCD engine.
-	params->dsi.data_format.color_order = LCM_COLOR_ORDER_RGB;
-	params->dsi.data_format.trans_seq = LCM_DSI_TRANS_SEQ_MSB_FIRST;
-	params->dsi.data_format.padding = LCM_DSI_PADDING_ON_LSB;
-	params->dsi.data_format.format = LCM_DSI_FORMAT_RGB888;
 
-	// Highly depends on LCD driver capability.
-	// Not support in MT6573
-	params->dsi.packet_size = 256;
+    params->dsi.LANE_NUM = LCM_FOUR_LANE;
+    /* The following defined the fomat for data coming from LCD engine. */
+    params->dsi.data_format.color_order = LCM_COLOR_ORDER_RGB;
+    params->dsi.data_format.trans_seq = LCM_DSI_TRANS_SEQ_MSB_FIRST;
+    params->dsi.data_format.padding = LCM_DSI_PADDING_ON_LSB;
+    params->dsi.data_format.format = LCM_DSI_FORMAT_RGB888;
 
-	// Video mode setting
-	params->dsi.PS = LCM_PACKED_PS_24BIT_RGB888;
-	params->dsi.word_count = FRAME_WIDTH * 3;
+    /* Highly depends on LCD driver capability. */
+    params->dsi.packet_size = 256;
+    /* video mode timing */
+    params->dsi.PS = LCM_PACKED_PS_24BIT_RGB888;
 
-	params->dsi.vertical_sync_active = 2;
-	params->dsi.vertical_backporch = 20;
-	params->dsi.vertical_frontporch = 22;
-	params->dsi.vertical_active_line = FRAME_HEIGHT;
+    params->dsi.vertical_sync_active = 2;
+    params->dsi.vertical_backporch = 254;
+    params->dsi.vertical_frontporch = 10;
+    params->dsi.vertical_active_line = FRAME_HEIGHT;
 
-	params->dsi.horizontal_sync_active = 10;
-	params->dsi.horizontal_backporch = 30;
-	params->dsi.horizontal_frontporch = 30;
-	params->dsi.horizontal_active_pixel = FRAME_WIDTH;
+    params->dsi.horizontal_sync_active = 4;
+    params->dsi.horizontal_backporch = 100;
+    params->dsi.horizontal_frontporch = 101;
+    params->dsi.horizontal_active_pixel = FRAME_WIDTH;
+    params->dsi.ssc_disable = 1;
+    /* this value must be in MTK suggested table */
+    params->dsi.PLL_CLOCK = 328;
+    /* clk continuous video mode */
+    params->dsi.cont_clock = 0;
 
-	params->dsi.PLL_CLOCK = 200; // 234
-
-	// params->dsi.ssc_disable = 1;
-	params->dsi.cont_clock = 1;
-
-	// params->dsi.clk_lp_per_line_enable   = 1;
-	params->dsi.esd_check_enable = 1;
-	params->dsi.customization_esd_check_enable = 1;
-	params->dsi.lcm_esd_check_table[0].cmd = 0x0a;
-	params->dsi.lcm_esd_check_table[0].count = 1;
-	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9c;
+    params->dsi.clk_lp_per_line_enable = 0;
+    if (get_boot_mode() == META_BOOT) {
+        boot_mode++;
+        LCM_LOGI("META_BOOT\n");
+    }
+    if (get_boot_mode() == ADVMETA_BOOT) {
+        boot_mode++;
+        LCM_LOGI("ADVMETA_BOOT\n");
+    }
+    if (get_boot_mode() == ATE_FACTORY_BOOT) {
+        boot_mode++;
+        LCM_LOGI("ATE_FACTORY_BOOT\n");
+    }
+    if (get_boot_mode() == FACTORY_BOOT)     {
+        boot_mode++;
+        LCM_LOGI("FACTORY_BOOT\n");
+    }
+    if (boot_mode == 0) {
+        LCM_LOGI("neither META_BOOT or FACTORY_BOOT\n");
+        params->dsi.esd_check_enable = 0;
+        params->dsi.customization_esd_check_enable = 0;
+        params->dsi.lcm_esd_check_table[0].cmd = 0x0A;
+        params->dsi.lcm_esd_check_table[0].count = 1;
+        params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9C;
+    }
+	
+	params->blmap = blmap_table;
+	params->blmap_size = sizeof(blmap_table)/sizeof(blmap_table[0]);
+	params->brightness_max = 4095;
+	params->brightness_min = 10;
 }
 
 static void lcm_init(void)
